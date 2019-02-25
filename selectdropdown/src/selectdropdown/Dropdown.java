530bc45d1b5de4999808d8a0098703afcca01674
Apeksha <Apeksha@192.168.0.25>
2019-02-25 13:14:14 -0500

    good changes

---
+++ selectdropdown/src/selectdropdown/Dropdown.java
@@ -0,0 +1,27 @@
+package selectdropdown;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class Dropdown {
+	@Test(priority=0)
+	public void first(){
+		System.out.println("first");
+	}
+@Test(priority=1)
+public void second(){
+	System.out.println("second");
+}
+@Test(priority=2)
+public void third(){
+	System.out.println("third");
+}
+
+}
